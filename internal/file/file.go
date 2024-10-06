c8dc9fd23a1bcd61959278f23c5afdb66b5eec5d
Konstantin Kuzminov <zal1358@gmail.com>
2024-10-06 14:15:35 +0200

    file changes

--- internal/file/file.go
+++ internal/file/file.go
@@ -7,3 +7,17 @@ type File struct {
 	Name string
 	Data []byte
 }
+
+func NewFile(filename string, blob []byte) (File, error) {
+	fileId, err := uuid.NewUUID()
+
+	if err != nil {
+		return File{}, err
+	}
+
+	return File{
+		ID:   fileId,
+		Name: filename,
+		Data: blob,
+	}, nil
+}
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,5 +1,7 @@
 package storage
 
+import "github.com/zalhonan/storage/internal/file"
+
 type Storage struct{}
 
 func NewStorage() *Storage {
@@ -8,4 +10,14 @@ func NewStorage() *Storage {
 
 func (s Storage) String() string {
 	return "I am the Storage"
+}
+
+func (s *Storage) Upload(filename string, blob []byte) (file.File, error) {
+	newFile, err := file.NewFile(filename, blob)
+
+	if err != nil {
+		return file.File{}, err
+	}
+
+	return newFile, nil
 }
